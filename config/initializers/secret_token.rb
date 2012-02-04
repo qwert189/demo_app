a1e9e913816ddff80e0e1d1fcfe6ca1a8379a92d
Fletcher Liverance <fletcher.liverance@gmail.com>
2012-02-04 17:01:00 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c7b0f00de7791c41552587d52c63be5c8a955c56c844c6f94eb6b95aa0093f1aae022020d138a2b49aec477522e54bef2924621f7ac68b430bf8f60b5e944022'
